b99a2496ab5da51d7ce8029050a75be6a9edb7b0
kc-lai <32350509+kc-lai@users.noreply.github.com>
2017-09-28 11:52:23 +0800

    Update text-processed.py

--- text-processed.py
+++ text-processed.py
@@ -6,15 +6,12 @@ jieba.load_userdict("userdict.txt")
 content = open('result.txt', 'rb').read()
 words = jieba.cut(content, cut_all=False)
 
-print "Output 精確模式 Full Mode："
-
 import jieba.analyse
 wordstop = jieba.analyse.extract_tags(content, topK=50)
 
 print "Output top 50："
 for word in wordstop:
     print word
-
 
 tags = jieba.analyse.extract_tags(content, 10)
 
